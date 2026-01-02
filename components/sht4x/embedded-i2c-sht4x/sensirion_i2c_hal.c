0dca1576bf3c50f756333e258a03fed86d78bf19
hyperimpose <hyperimpose@protonmail.com>
2026-01-02 02:55:56 +0200

    Replace embedded-i2c-sht4x/sensirion_i2c_hal.c with out own implementation

--- components/sht4x/embedded-i2c-sht4x/sensirion_i2c_hal.c
+++ components/sht4x/embedded-i2c-sht4x/sensirion_i2c_hal.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2018, Sensirion AG
+ * Copyright (c) 2026, hyperimpose.org
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,9 +30,80 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2c_master.h"
+#include "driver/i2c_types.h"
+#include "esp_rom_sys.h"
+
 #include "sensirion_i2c_hal.h"
 #include "sensirion_common.h"
 #include "sensirion_config.h"
+
+
+/*
+ * ESP-IDF Implementation Notes
+ * ============================
+ *
+ * The HAL API as defined in sensirion_i2c_hal.h includes the following:
+ * - sensirion_i2c_hal_select_bus/1
+ * - sensirion_i2c_hal_init/0
+ * - sensirion_i2c_hal_free/0
+ * Those functions are not used internally by the driver and they seem to only
+ * exist as an external API for the users of the library.
+ *
+ * Bus lifecycle and device registration are handled by the user application to
+ * allow for shared bus configurations. To do this, we introduce two functions:
+ * - sensirion_i2c_hal_device_init/1
+ * - sensirion_i2c_hal_device_free/0
+ *
+ * Devices handles
+ * ---------------
+ * The device used by the driver is set in the pointer `*sht4x_dev_handle'.
+ *
+ * Use `sensirion_i2c_hal_device_init/1' to set the pointer to the active
+ * I2C device handle. Use `sensirion_i2c_hal_device_free/0' to unset it.
+ *
+ * You MUST free the pointer before setting it to a new device.
+ *
+ * Thread safety
+ * -------------
+ * The pointer `*sht4x_dev_handle' is protected by a mutex.
+ *
+ * Calling `sensirion_i2c_hal_device_init/1' sets the mutex and blocks any
+ * further calls until `sensirion_i2c_hal_device_free/0' is called.
+ *
+ * ---
+ *
+ * The read/write functions are thread safe according to the ESP-IDF docs.
+ * This means that you can query the same device from different threads.
+ *
+ * See: https://docs.espressif.com/projects/esp-idf/en/v5.5.2/esp32c6
+ *                                /api-reference/peripherals
+ *                                /i2c.html#thread-safety
+ *
+ * Ignored arguments
+ * -----------------
+ * The `address' parameter in read/write functions is ignored because the
+ * ESP-IDF handle already contains the target address and bus context.
+ */
+
+static i2c_master_dev_handle_t *sht4x_dev_handle = NULL;
+static SemaphoreHandle_t sht4x_dev_mutex = NULL;
+
+
+void sensirion_i2c_hal_device_init(i2c_master_dev_handle_t* handle) {
+    if (sht4x_dev_mutex)
+        if (xSemaphoreTake(sht4x_dev_mutex, portMAX_DELAY) == pdTRUE)
+            sht4x_dev_handle = handle;
+}
+
+void sensirion_i2c_hal_device_free(void) {
+    sht4x_dev_handle = NULL;
+    if (sht4x_dev_mutex)
+        xSemaphoreGive(sht4x_dev_mutex);
+}
+
 
 /*
  * INSTRUCTIONS
@@ -52,9 +124,6 @@
  * @returns         0 on success, an error code otherwise
  */
 int16_t sensirion_i2c_hal_select_bus(uint8_t bus_idx) {
-    /* TODO:IMPLEMENT or leave empty if all sensors are located on one single
-     * bus
-     */
     return NOT_IMPLEMENTED_ERROR;
 }
 
@@ -63,14 +132,19 @@ int16_t sensirion_i2c_hal_select_bus(uint8_t bus_idx) {
  * communication.
  */
 void sensirion_i2c_hal_init(void) {
-    /* TODO:IMPLEMENT */
+    if (sht4x_dev_mutex == NULL) {
+        sht4x_dev_mutex = xSemaphoreCreateMutex();
+    }
 }
 
 /**
  * Release all resources initialized by sensirion_i2c_hal_init().
  */
 void sensirion_i2c_hal_free(void) {
-    /* TODO:IMPLEMENT or leave empty if no resources need to be freed */
+    if (sht4x_dev_mutex != NULL) {
+        vSemaphoreDelete(sht4x_dev_mutex);
+        sht4x_dev_mutex = NULL;
+    }
 }
 
 /**
@@ -84,8 +158,11 @@ void sensirion_i2c_hal_free(void) {
  * @returns 0 on success, error code otherwise
  */
 int8_t sensirion_i2c_hal_read(uint8_t address, uint8_t* data, uint8_t count) {
-    /* TODO:IMPLEMENT */
-    return NOT_IMPLEMENTED_ERROR;
+    if (sht4x_dev_handle == NULL) {
+        return -1;
+    }
+    esp_err_t ret = i2c_master_receive(*sht4x_dev_handle, data, count, -1);
+    return (ret == ESP_OK) ? 0 : -1;
 }
 
 /**
@@ -101,8 +178,11 @@ int8_t sensirion_i2c_hal_read(uint8_t address, uint8_t* data, uint8_t count) {
  */
 int8_t sensirion_i2c_hal_write(uint8_t address, const uint8_t* data,
                                uint8_t count) {
-    /* TODO:IMPLEMENT */
-    return NOT_IMPLEMENTED_ERROR;
+    if (sht4x_dev_handle == NULL) {
+        return -1;
+    }
+    esp_err_t ret = i2c_master_transmit(*sht4x_dev_handle, data, count, -1);
+    return (ret == ESP_OK) ? 0 : -1;
 }
 
 /**
@@ -114,5 +194,14 @@ int8_t sensirion_i2c_hal_write(uint8_t address, const uint8_t* data,
  * @param useconds the sleep time in microseconds
  */
 void sensirion_i2c_hal_sleep_usec(uint32_t useconds) {
-    /* TODO:IMPLEMENT */
+    /* NOTE: esp_rom_delay_us is part of the esp-idf internal API.
+     *
+     * Using vTaskDelay alone also works fine.
+     */
+    if (useconds < 10000) {
+        esp_rom_delay_us(useconds);
+    } else {
+        uint32_t msec = (useconds + 999) / 1000;
+        vTaskDelay(pdMS_TO_TICKS(msec));
+    }
 }
